aede321c496d9374486f7b5339f8fb1780a65779
Gagana523 <gaganaanuhas321@gmail.com>
2026-03-06 11:31:48 +0530

    Fix syntax error in pair.js

--- pair.js
+++ pair.js
@@ -588,7 +588,7 @@ END:VCARD`
         console.error('TikTok Search Error:', err);
         await socket.sendMessage(sender, { text: `❌ Error: ${err.message}` }, { quoted: shonux });
     }
-    break;
+    break;}
 
     
 
